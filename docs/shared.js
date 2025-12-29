9639f8340496aa82e3487d69dd43042647e6717b
lnagy2002 <kavoi2011@gmail.com>
2025-12-29 15:41:07 -0800

    Fix URL in loadData function

--- docs/shared.js
+++ docs/shared.js
@@ -1,6 +1,6 @@
 async function loadData() {
     try {
-        const res = await fetch('https://dpenati.github.io/portofolio/usecases.json'); // <-- your JSON file
+        const res = await fetch('https://dpenati.github.io/portfolio/usecases.json'); // <-- your JSON file
         if (!res.ok) throw new Error(`HTTP ${res.status}`);
         articles = await res.json();  
         render();
