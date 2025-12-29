ff3ba48426264bb804fa731faed424fdc5f86897
lnagy2002 <kavoi2011@gmail.com>
2025-12-29 15:49:48 -0800

    Log fetched articles to console
    Added console log to display fetched articles.

--- docs/shared.js
+++ docs/shared.js
@@ -3,6 +3,7 @@ async function loadData() {
         const res = await fetch('https://dpenati.github.io/portfolio/usecases.json'); // <-- your JSON file
         if (!res.ok) throw new Error(`HTTP ${res.status}`);
         articles = await res.json();  
+        console.log (articles);
         render();
     } catch (err) {
       console.error  (err);
