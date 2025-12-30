a38664b24f345cb5096daee3e1eb55d12127bb9e
lnagy2002 <kavoi2011@gmail.com>
2025-12-29 16:11:45 -0800

    Rename loadDUseCases to loadUseCases

--- docs/shared.js
+++ docs/shared.js
@@ -1,4 +1,4 @@
-async function loadDUseCases() {
+async function loadUseCases() {
     try {
         const res = await fetch('https://dpenati.github.io/portfolio/useCases/data.json'); // <-- your JSON file
         if (!res.ok) throw new Error(`HTTP ${res.status}`);
