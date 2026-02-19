a5ce81968d8a68ab0cab430d8e18524204aa4606
dpenati <daniela@dpenati.com>
2026-02-18 17:13:59 -0800

    Rotate Firebase browser API key

--- docs/auth-config.js
+++ docs/auth-config.js
@@ -3,7 +3,7 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-aut
 
 // Paste your Firebase config here (from Firebase console -> Web app)
 const firebaseConfig = {
-  apiKey: 'AIzaSyAsGJ1uWJyYjqAZKk-7rkjqPoFUa6xH0ws',
+  apiKey: 'AIzaSyBUZzQwgo_j1-eL6p3jiq_gSAOxBSUjHQk',
   authDomain: 'myportfolio-f9b16.firebaseapp.com',
   projectId: 'myportfolio-f9b16',
   appId: '1:288033050147:web:358ae22b206e46cfd82b96',
